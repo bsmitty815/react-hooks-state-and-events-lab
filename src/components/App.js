be7096f9e9748f6c9340b08940659f0f0430b61e
Brian Smith <brian.bubbam@gmail.com>
2021-06-16 16:22:52 -0500

    commit tree eba17506db72d4b8f53c470634f8e142514406fc

--- src/components/App.js
+++ src/components/App.js
@@ -4,15 +4,20 @@ import itemData from "../data/items";
 
 function App() {
   const [items, setItems] = useState(itemData);
+  const [isOn, setIsOn] = useState(false)
 
+  function ButtonModeClickHandler() {
+    setIsOn((isOn) => !isOn)
+    
+  }
   // this data will be passed down to the ShoppingList as a prop
-  console.log(items);
+
 
   return (
-    <div className={"App " + (false ? "dark" : "light")}>
+    <div className={"App " + (isOn ? "dark" : "light")}>
       <header>
         <h2>Shopster</h2>
-        <button>Dark Mode</button>
+        <button onClick={ButtonModeClickHandler}>{isOn ? "Light Mode" : "Dark Mode"}</button>
       </header>
       <ShoppingList items={items} />
     </div>
